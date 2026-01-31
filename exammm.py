5fe2dd2d86d1dca396a03ba4e485b15282089c96
salarahmad404 <salar404muassib@gmail.com>
2026-01-31 21:12:46 +0500

    Add files via upload

---
+++ exammm.py
@@ -0,0 +1,13 @@
+name="sayyat"
+age=21
+country="Egypt"
+print("My name is",name)
+print("My age is",age)
+print("My country is",country)
+print("My name is "+name+", my age is "+str(age)+", and my country is "+country)
+print("Data type of name is",type(name))
+print("Data type of age is",type(age))
+print("Data type of country is",type(country))
+print("\n After casting...")
+age=str(age)
+print("Data type of age is",type(age))
